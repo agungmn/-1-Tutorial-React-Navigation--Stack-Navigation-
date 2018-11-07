29eea67a4aa9ccd745824a540c952ac4b6df140f
Agung Muhamad <44847075+agungmn@users.noreply.github.com>
2018-11-08 01:31:40 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { createStackNavigator} from 'react-navigation'
+import LoginScreen from './screens/LoginScreen'
+import HomeScreen from './screens/HomeScreen'
+export default class App extends React.Component{
+  render(){
+    return(
+      <AppStackNavigator/>
+      );
+  }
+}
+
+const AppStackNavigator=createStackNavigator({
+  Login:LoginScreen,
+  Home:HomeScreen
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
